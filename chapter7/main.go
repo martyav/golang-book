73d7c0c5aa435406c1700ccf016675de5dfca955
Marty Hernandez Avedon <martyavedon@gmail.com>
2019-04-05 11:46:08 -0400

    recursion and variadics

--- chapter7/main.go
+++ chapter7/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+/*
+	Go allows functions to return multiple things
+*/
+
 func isValueInAndWhere(arr []string, value string) (bool, int) {
 	for i, text := range arr {
 		if value == text {
@@ -13,22 +17,47 @@ func isValueInAndWhere(arr []string, value string) (bool, int) {
 }
 
 /*
-	Go allows functions to return multiple things
+	Go also has variadic functions
 */
 
+func variadic(args ...string) int {
+	return len(args)
+}
+
 func main() {
+	length := variadic("one", "**", "3", "IV", "|||||")
+
+	fmt.Println("The length of all these items is", length)
+
 	texts := []string{"bunny", "chick", "calf"}
 	shouldBeFalse, shouldBeNegativeOne := isValueInAndWhere(texts, "puppy")
 	shouldBeTrue, shouldBeOne := isValueInAndWhere(texts, "chick")
 
-	fmt.Println(shouldBeFalse, shouldBeNegativeOne)
-	fmt.Println(shouldBeTrue, shouldBeOne)
-
-	add := func(x, y int) int { return x + y }
+	fmt.Println("Should be false, should be -1:", shouldBeFalse, shouldBeNegativeOne)
+	fmt.Println("Should be true, should be 1:", shouldBeTrue, shouldBeOne)
 
 	/*
 		We can define functions within functions by using closures
 	*/
 
-	fmt.Println(add(2, 2))
+	add := func(x, y int) int { return x + y }
+
+	fmt.Println("2 + 2 =", add(2, 2))
+
+	fmt.Println(numerology(10001))
+}
+
+/*
+	Go also supports recursion.
+
+	Note that Go will not allow you to define a named function inside another
+	named function!
+*/
+
+func numerology(num int) int {
+	if num < 10 {
+		return num
+	}
+
+	return numerology(num/10) + (num % 10)
 }
